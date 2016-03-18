3af89f36bf06c2c3e7944adb95f7ee15eb1147ab
Mindaugas Liubinas <mindaugas.l@estina.lt>
2016-03-18 23:48:14 +0200

    Removed GTM
    Summary: Removed GTM
    
    Test Plan: no plan
    
    Reviewers: gf
    
    Reviewed By: gf
    
    Differential Revision: http://mano.gerasfabrikas.lt/D3

--- views/header.php
+++ views/header.php
@@ -26,24 +26,6 @@ endif;
 		<script src="/clamp.min.js"></script>
 		<script src="/script.js"></script>
 		<!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="/styleIE.css" /><![endif]-->
-<!-- Google Tag Manager -->
-
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KXJ5V5"
-
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-
-})(window,document,'script','dataLayer','GTM-KXJ5V5');</script>
-
-<!-- End Google Tag Manager -->
-
 
 <!-- Facebook Pixel Code -->
 <script>
